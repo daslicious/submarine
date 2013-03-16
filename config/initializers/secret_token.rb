3d8bb5b923d7e75ced48d1385127b11d59d6d8df
damien jones <damiennews@gmail.com>
2013-03-16 11:00:45 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Submarine::Application.config.secret_token = 'eec0a6acf63e64355f916b60f8ae83b5d6d43ca09f351e7dc336f1356a3b8aa05067671ea88396ebac387ad7d2c831bb3d8c5b9849bdfe60608fc9a93169295f'
